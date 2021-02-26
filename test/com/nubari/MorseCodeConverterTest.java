f44ad5bd848f25cbb3f19b6cda16fb56e6a325d0
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-02-26 22:49:46 +0100

    updated tests

--- test/com/nubari/MorseCodeConverterTest.java
+++ test/com/nubari/MorseCodeConverterTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MorseCodeConverterTest {
-    MorseCodeConverter morseCodeConverter;
 
     @BeforeEach
     void setUp() {
